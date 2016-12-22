386b6907d28e45b9878516dfe452ae29e4d921f1
Peter Lu <peterlu6@stanford.edu>
2016-12-22 15:19:04 -0800

    added path

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var p = require('path');
 var app = express();
 
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/index.html');
+  response.sendFile(p.join(__dirname + '/index.html'));
 });
+
+app.listen(8080);
