3a1c710c72617bef2c1fc9684bb2b51f1b35c79f
Peter Lu <peterlu6@stanford.edu>
2016-12-22 17:16:49 -0800

    added some things

--- loginhandling.js
+++ loginhandling.js
@@ -1,17 +1,26 @@
 window.onload = getData;
 
-var foundcity;
-var foundstate;
-var foundcountry;
-
-var locationMap = {};
-locationMap["Vancouver,WA,US"] = "Peter";
+function getIdentity(foundcity, foundstate, foundcountry){
+	if(foundcountry == "IN") return "Somya";
+	if(foundstate == "WA") return "Peter";
+	if(foundstate == "MD") return "Annie";
+	if(foundstate == "TX") return "Lewis";
+	if(foundstate == "NY") return "Ashley";
+	if(foundstate == "CA") return "Madison";
+}
 
 function getData() {
-  foundcity = geoplugin_city();
-  foundstate = geoplugin_region();
-  foundcountry = geoplugin_countryCode();
+  var foundcity = geoplugin_city();
+  var foundstate = geoplugin_region();
+  var foundcountry = geoplugin_countryCode();
+  var user = getIdentity(foundcity, foundstate, foundcountry);
+  var targets = ["Peter", "Lewis", "Annie", "Somya", "Madison", "Ashley"];
+  var randomized = randomize(targets);
+  var targetIndex = (randomized.indexOf(user) + 1) % targets.length;
+
   var assigned = document.getElementById('assignment');
-  assigned.innerHTML = foundcity + "," + foundstate + "," + foundcountry;
+  var welcome = document.getElementById('welcome');
+  welcome.innerHTML = "Welcome " + user + "!";
+  assigned.innerHTML = "your target is: " + randomized[targetIndex];
 }
 
