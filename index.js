0b78fa8fbb718d702ecab7b3ba0c06a13571d348
Peter Lu <peterlu6@stanford.edu>
2016-12-22 16:00:37 -0800

    new script

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ var app = express();
 app.get("/", function (request, response) {
   response.sendFile(p.join(__dirname + '/index.html'));
 });
+app.get("/loginhandling.js", function (request, response) {
+  response.sendFile(__dirname + '/loginhandling.js');
+});
 
 app.listen(process.env.PORT || 5000)
